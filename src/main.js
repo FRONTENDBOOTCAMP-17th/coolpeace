cbd9b85fcfcd2ae4ef28a125bd3dd9617d904cac
harikim02 <yeonsoo020614@gmail.com>
2026-02-05 11:23:17 +0900

    index.html에 각자 만든 페이지 연결 (About, Download)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-import "./style.css"
+import './style.css';
+
+async function loadHTML(id, path) {
+  const res = await fetch(path);
+  const html = await res.text();
+  document.querySelector(id).innerHTML = html;
+}
+
+// loadHTML('#banner', '/src/banner/banner.html');
+loadHTML('#about', '/src/about/about.html');
+loadHTML('#download', '/src/download/download.html');
+// loadHTML('#qna', '/src/qna/qna.html');
+// loadHTML('#apply', '/src/apply/apply.html');
+// loadHTML('#footer', '/src/footer/footer.html');
